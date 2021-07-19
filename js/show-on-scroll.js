3caf583268751e13125ef9f8a873d19eb0e3a63d
hugooleal <77586151+hugooleal@users.noreply.github.com>
2021-07-18 23:03:57 -0300

    Fix readme.

--- js/show-on-scroll.js
+++ js/show-on-scroll.js
@@ -1,16 +1,39 @@
-const targets = document.querySelectorAll('.show-on-scroll');
-const offset = (window.innerHeight * 0.8) + 100;
-console.log('offset' + offset);
-function showOnScroll(){
-    let documentTop = document.documentElement.scrollTop;
-    console.log('documentTop' + documentTop);
-    targets.forEach( (target) => {
-        let targetTop = target.offsetTop;
-        console.log('targetTop' + targetTop);
-        console.log('offset' + offset);
-        documentTop > targetTop - offset ? target.classList.add('is-visible') : target.classList.remove('is-visible');
-    });
+const scroll = window.requestAnimationFrame ||
+             // IE Fallback
+             function(callback){ window.setTimeout(callback, 1000/60)};
+const elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+
+function loop() {
+
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('is-visible');
+    } else {
+      element.classList.remove('is-visible');
+    }
+  });
+
+  scroll(loop);
 }
 
-showOnScroll();
-document.addEventListener('scroll', showOnScroll);
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  const rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Desafio Landing Page - Hiring Coders
+
+O site foi desenvolvido durante o desafio do programa Hiring Coders.
+
+O desafio foi criar uma landing page de promoções com temática de Black Friday.
+
+Para tanto, a landing page precisava contar com um campo para cadastro de email para recebimento de promoções.
+
+O email cadastrado precisava ser armazenado no Local Storage do navegador.
+
+Para visualizar o site online [clique aqui](https://eager-varahamihira-e9037f.netlify.app/)!
